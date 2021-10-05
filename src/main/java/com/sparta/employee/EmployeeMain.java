74a25bbb14ca198697cb896ebc12cc5906501db0
Dwhite77 <dan_white18@live.com>
2021-10-05 11:14:08 +0100

    added the csv file reading and began to implement spliting of each string into an array

--- src/main/java/com/sparta/employee/EmployeeMain.java
+++ src/main/java/com/sparta/employee/EmployeeMain.java
@@ -1,7 +1,48 @@
 package com.sparta.employee;
 
+import java.io.*;
+
 public class EmployeeMain {
     public static void main(String[] args) {
-        
+        String line = null;
+        String[] fields = null;
+        try (BufferedReader in = new BufferedReader(new FileReader("EmployeeRecords.csv"))) { // were going to auto open and close them with this(autocloseable interface), (try with resources)
+            while ((line = in.readLine()) != null) {
+                fields = line.split(",");
+                for(String i: fields)
+                    System.out.println(i);
+                System.out.println("-----------------------------");
+                //this is where we want to create the employee objects and then add them to the collection
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        catch(FileNotFoundException e){ e.printStackTrace();}
+        catch (IOException e) { e.printStackTrace(); }
+
+
     }
 }
