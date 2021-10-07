aa9ed54837f35a229e9f7c3fa4b20f2abc375fd6
Dwhite77 <dan_white18@live.com>
2021-10-07 16:59:06 +0100

    added in timing for the creation of employee objects

--- src/main/java/com/sparta/employee/EmployeeMain.java
+++ src/main/java/com/sparta/employee/EmployeeMain.java
@@ -11,7 +11,7 @@ public class EmployeeMain {
         String[] fields = null;
         long start;
         long end;
-
+        start = System.currentTimeMillis();
         ArrayList<EmployeeObject> employeeArrayList = new ArrayList<EmployeeObject>();
         ArrayList<String[]> odditites = new ArrayList<>();
         ArrayList<EmployeeObject> duplicateArrayList = new ArrayList<EmployeeObject>();
@@ -26,16 +26,22 @@ public class EmployeeMain {
                 try {
                     int employeeID = Integer.parseInt(fields[0]);
                     if (employeeID < 0) dataGate = false;
+
                     String prefix = fields[1];
                     if (prefix.length() > 5 || !regPat.regexPrefix(prefix)) dataGate = false;
+
                     String firstName = fields[2];
                     if (firstName.length() > 50 || !regPat.regexName(firstName)) dataGate = false;
+
                     String middleInitial = fields[3];
                     if (middleInitial.length() > 3 || !regPat.regexMiddleInital(middleInitial)) dataGate = false;
+
                     String lastName = fields[4];
                     if (lastName.length() > 50 || !regPat.regexName(lastName)) dataGate = false;
+
                     String gender = fields[5];
                     if (!regPat.regexGender(gender)) dataGate = false;
+
                     String email = fields[6];
                     if (!regPat.regexEmail(email)) dataGate = false;
 
@@ -44,14 +50,16 @@ public class EmployeeMain {
 
                     java.util.Date doe = new SimpleDateFormat("MM/dd/yyyy").parse(fields[8]);
                     java.sql.Date dateOfEmployment = new java.sql.Date(doe.getTime());
+
                     int salary  = Integer.parseInt(fields[9]);
                     if (salary < 0) dataGate = false;
+
                     if(dataGate){
                     boolean dupeGate = true;
                         EmployeeObject currentEmp = new EmployeeObject(employeeID,prefix,firstName,middleInitial,lastName,gender,email,dateOfBirth,dateOfEmployment,salary);
 
                         for(int i =0; i< employeeArrayList.size();i++){
-                            if(currentEmp.equals(employeeArrayList.get(i))){
+                            if(currentEmp.equals(employeeArrayList.get(i))){ // maybe try to implement some sort of hashmap here to speed it up?
                                 duplicateArrayList.add(currentEmp);
                                 dupeGate = false;
                                 break;
@@ -63,8 +71,7 @@ public class EmployeeMain {
                     }
                     else{
                         odditites.add(fields);
-                        //System.out.println(fields[0]+" "+fields[1]+" "+fields[2]+" "+fields[3]+" "+fields[4]+" "+fields[5]+" "+fields[6]+" "+fields[7]+" "+fields[8]+" "+fields[9]);
-                        // this allows me to check what the errors are
+
                     }
                 }
                 catch(Exception e){
@@ -77,8 +84,9 @@ public class EmployeeMain {
 
         catch(FileNotFoundException e){ e.printStackTrace();}
         catch (IOException e) { e.printStackTrace(); }
-
-        System.out.println("Valid entires: "+employeeArrayList.size() +"\nDuplicates: "+ duplicateArrayList.size()+"\nOddities: "+odditites.size());
+        end = System.currentTimeMillis();
+        System.out.println("Time to create employee objects: "+ (end-start)+"(ms)");
+        System.out.println("Valid entires: "+employeeArrayList.size() +"\nDuplicates: "+ duplicateArrayList.size()+"\nCorrupt Data: "+odditites.size());
 
         int empLen = employeeArrayList.size();
 
